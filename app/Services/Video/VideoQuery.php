c4dac2271dbef025635ec0e57ff367c8ef92148a
khalidmk1 <103368578+khalidmk1@users.noreply.github.com>
2024-06-05 10:58:06 +0100

    uodate get video

--- app/Services/Video/VideoQuery.php
+++ app/Services/Video/VideoQuery.php
@@ -132,7 +132,7 @@ class VideoQuery extends GlobaleService{
     //api video
 
     public function getVideoByContent(String $video){
-        $videos = ContentVideo::where('contentId' , $video)->get(['title' , 'video' , 'description']);
+        $videos = ContentVideo::where('contentId' , $video)->get(['id' ,'title' , 'video' , 'description']);
         return $videos;
     }
 
