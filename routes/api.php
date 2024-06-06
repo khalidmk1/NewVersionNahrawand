0d2a5f70e8348497e93a39e4691032e7117db7d4
khalidmk1 <103368578+khalidmk1@users.noreply.github.com>
2024-06-06 15:13:16 +0100

    change position

--- routes/api.php
+++ routes/api.php
@@ -32,9 +32,10 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [AuthenticatedSessionController::class, 'destroyApi'])->name('logout');
     Route::post('update/password', [PasswordController::class, 'updateApi'])->name('update.password');
 
-    Route::post('profile/update', [ProfileController::class, 'updateApi'])->name('user.update');
+    
     Route::post('profile/avatar/update', [ProfileController::class, 'updateAvatar'])->name('avatar.update');
-
+    Route::post('profile/update', [ProfileController::class, 'updateApi'])->name('user.update');
+    
     Route::get('client', [ProfileController::class, 'authUser'])->name('user.index');
     
     Route::post('create/favoris/{content}', [ContentController::class, 'createFavoris'])->name('favoris.create');
