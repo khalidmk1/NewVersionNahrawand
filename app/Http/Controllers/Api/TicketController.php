922a92ac77d914e14fc9977363a66f9bb0939a49
khalidmk1 <103368578+khalidmk1@users.noreply.github.com>
2024-06-06 12:43:36 +0100

    update ticketController

--- app/Http/Controllers/Api/TicketController.php
+++ app/Http/Controllers/Api/TicketController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Interfaces\TicketInterface;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TicketRequest;
 
 class TicketController extends Controller
 {
@@ -15,7 +16,7 @@ class TicketController extends Controller
         $this->TicketInterface = $TicketInterface;
     }
 
-    public function create(){
+    public function create(TicketRequest $request){
         $this->TicketInterface->create(); 
     }
 }
