9d78d8fb7b58b8c512da060036530c964c356059
khalidmk1 <103368578+khalidmk1@users.noreply.github.com>
2024-06-07 12:57:56 +0100

    get user subcategory

--- app/Services/Profile/ProfileQeury.php
+++ app/Services/Profile/ProfileQeury.php
@@ -241,7 +241,7 @@ class ProfileQeury extends GlobaleService {
     } 
 
     public function allUserSubcatagoryApi(){
-        $userSubCategory = UserSubcategory::where('userId' , Auth::user()->id);
+        $userSubCategory = UserSubcategory::where('userId' , Auth::user()->id)->get();
         return $userSubCategory;
     }
 
