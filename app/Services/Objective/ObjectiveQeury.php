49007d869f2f7ed2039686a66f06d735a50a1783
khalidmk1 <103368578+khalidmk1@users.noreply.github.com>
2024-07-08 12:43:24 +0100

    hhh

--- app/Services/Objective/ObjectiveQeury.php
+++ app/Services/Objective/ObjectiveQeury.php
@@ -29,14 +29,22 @@ class ObjectiveQeury extends GlobaleService {
 
  */
 
- public function allObjectives() {
-    $objectives = UserObjective::select('subCategoryId', DB::raw('MIN(id) as id'))
-        ->where('userId', Auth::user()->id)
-        ->groupBy('subCategoryId')
-        ->get();
-
-    return $objectives;
-}
+    public function allObjectives() {
+        $userId = Auth::user()->id;
+    
+        $subQuery = UserObjective::select(DB::raw('MIN(id) as min_id'))
+            ->where('userId', $userId)
+            ->groupBy('subCategoryId');
+        
+        $objectives = UserObjective::select('user_objectives.*')
+            ->joinSub($subQuery, 'min_ids', function ($join) {
+                $join->on('user_objectives.id', '=', 'min_ids.min_id');
+            })
+            ->where('userId', $userId)
+            ->get();
+        
+        return $objectives;
+    }
 
     public function storeObjective(ObjectiveRequest $request ,String $contentId)
     {
