79efbe118ae760b78aa094d2c7daddec7c2e70f6
khalidmk1 <103368578+khalidmk1@users.noreply.github.com>
2024-07-12 15:15:55 +0100

    handle expession

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Throwable;
 
 class Handler extends ExceptionHandler
